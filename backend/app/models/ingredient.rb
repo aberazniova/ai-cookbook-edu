6f76b55ac10f5c46cff7195b2b21eb1096c78e9c
Anastastasia-B <anastasiaberz212@gmail.com>
2025-07-07 14:13:43 +0000

    auto annotate models

--- backend/app/models/ingredient.rb
+++ backend/app/models/ingredient.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: ingredients
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  name       :string(255)      not null
+#  recipe_id  :integer          not null
+#
+# Indexes
+#
+#  index_ingredients_on_recipe_id  (recipe_id)
+#
+
 class Ingredient < ApplicationRecord
   belongs_to :recipe
 end
--- backend/app/models/recipe.rb
+++ backend/app/models/recipe.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id           :integer          not null, primary key
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  title        :string(255)      not null
+#  instructions :text(65535)      not null
+#
+
 class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
 end
