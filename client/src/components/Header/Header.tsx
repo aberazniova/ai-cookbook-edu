dff8fc5df3d112cb28270d9c124f1ea2d151056b
Anastastasia-B <anastasiaberz212@gmail.com>
2025-07-09 13:21:12 +0000

    redirect to single recipe page when recipe card is clicked

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,17 +1,20 @@
 import { Navbar, NavbarBrand, NavbarToggle, Avatar } from "flowbite-react";
 import { FaUtensils } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="bg-white dark:bg-neutral-900 shadow-xl">
       <div className="container mx-auto px-2 sm:px-4 lg:px-8">
         <Navbar fluid rounded={false} className="bg-white dark:bg-neutral-900 py-4 lg:py-5">
-          <NavbarBrand href="#">
-            <span className="self-center whitespace-nowrap text-3xl lg:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r
+          <NavbarBrand>
+            <Link to="/" className="flex items-center">
+              <span className="self-center whitespace-nowrap text-3xl lg:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r
  from-emerald-600 to-orange-500 dark:from-emerald-400 dark:to-orange-300 mr-2">
-              The Cookbook
-            </span>
-            <FaUtensils className="text-xl lg:text-2xl text-orange-500 dark:text-orange-300" />
+                The Cookbook
+              </span>
+              <FaUtensils className="text-xl lg:text-2xl text-orange-500 dark:text-orange-300" />
+            </Link>
           </NavbarBrand>
           <div className="flex md:order-2">
             <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
