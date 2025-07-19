6553815fd0360b6d8de98d5d254a4803ae04e662
Anastastasia-B <anastasiaberz212@gmail.com>
2025-07-19 22:58:53 +0000

    limit the history lenght passed to gemini

--- backend/app/services/chatbot/build_payload/conversation_history.rb
+++ backend/app/services/chatbot/build_payload/conversation_history.rb
@@ -8,12 +8,17 @@ module Chatbot
       end
 
       def call
-        conversation.conversation_turns.order(created_at: :asc).map(&:payload)
+        conversation.conversation_turns.order(created_at: :asc).limit(max_history_length).map(&:payload)
       end
 
       private
 
       attr_reader :conversation
+
+      # This limit exists for minimizing Gemini API token usage
+      def max_history_length
+        ENV.fetch("GEMINI_API_MAX_HISTORY_LENGTH", 10).to_i
+      end
     end
   end
 end
