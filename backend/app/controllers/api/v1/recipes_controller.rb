d9fee678b396fd554eb8a0ee6a943fbe0d2ee3e4
Anastastasia-B <anastasiaberz212@gmail.com>
2025-07-21 21:41:54 +0000

    display recipes from newest to oldest

--- backend/app/controllers/api/v1/recipes_controller.rb
+++ backend/app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order(created_at: :desc)
     render json: Resources::RecipesList.call(@recipes)
   end
 
