b9a043a2bd3428b8f016541fac478df9849426df
Anastasiya Berazniova <anastasiaberz212@gmail.com>
2025-08-15 20:14:42 +0200

    enhance rubocop rules

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,12 +11,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :auth do
         devise_for :users,
-                        path: "",
-                        path_names: { sign_in: "sign_in", sign_out: "sign_out", registration: "sign_up" },
-                        controllers: {
-                          sessions: "api/v1/auth/sessions",
-                          registrations: "api/v1/auth/registrations"
-                        }
+          path: "",
+          path_names: { sign_in: "sign_in", sign_out: "sign_out", registration: "sign_up" },
+          controllers: {
+            sessions: "api/v1/auth/sessions",
+            registrations: "api/v1/auth/registrations"
+          }
       end
 
       resources :recipes, only: [:index, :show]
